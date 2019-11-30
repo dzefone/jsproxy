7467d95c28d1acf2623f2d5d1721a8d52da06cde
dzefone <zhangzhifeng.007@gmail.com>
2019-11-30 13:44:38 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -11,21 +11,7 @@ jsproxy_config({
   },
 
   // 节点配置
-  node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
+  node_map: {    
     'mysite': {
       label: '当前站点',
       lines: {
@@ -65,10 +51,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/dzefone/jsproxy@gh-pages/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  assets_cdn: 'assets/',
+  //assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
@@ -87,12 +73,6 @@ jsproxy_config({
   url_handler: {
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
-    'http://haha.com/': {
-      content: 'Hello World'
-    },
+    }    
   }
 })
